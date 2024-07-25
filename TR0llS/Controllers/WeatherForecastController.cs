42d8aa192d58a4555b697ba3566726ec0962c90d
Monorom <monoroms@hotmail.com>
2024-07-25 23:33:09 +0200

    Test

--- TR0llS/Controllers/WeatherForecastController.cs
+++ TR0llS/Controllers/WeatherForecastController.cs
@@ -18,8 +18,8 @@ namespace TR0llS.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
